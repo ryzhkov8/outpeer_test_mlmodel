54d88793d17367ef239c5ee8143de7f159b79a47
Nikita R <ryzhkov984@gmail.com>
2025-02-14 20:12:34 +0500

    рефакторинг кода

--- app.py
+++ app.py
@@ -2,17 +2,14 @@ from fastapi import FastAPI
 import joblib
 import pandas as pd
 
-# Загрузка обученной модели
-model = joblib.load("iris_model.pkl")  # Сохраните модель заранее
+model = joblib.load("iris_model.pkl")  
 
 app = FastAPI()
 
 @app.post("/predict/")
 async def predict(sepal_length: float, sepal_width: float, petal_length: float, petal_width: float):
-    # Формируем входные данные для модели
     input_data = pd.DataFrame([[sepal_length, sepal_width, petal_length, petal_width]], 
                               columns=["sepal_length", "sepal_width", "petal_length", "petal_width"])
     
-    # Делаем предсказание
     prediction = model.predict(input_data)[0]
     return {"species": prediction}
--- model.py
+++ model.py
@@ -10,19 +10,15 @@ data = pd.read_csv("iris.csv")
 X = data.drop(columns=["species"])
 y = data["species"]
 
-# 2. Разделение данных на обучающую и тестовую выборки
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# 3. Настройка и обучение модели
 model = RandomForestClassifier(random_state=42)
 model.fit(X_train, y_train)
 
-# 4. Оценка модели
 y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy}")
 
-# 5. Логирование метрик и модели с помощью MLflow
 mlflow.set_experiment("Iris Classification")
 
 with mlflow.start_run():
@@ -34,6 +30,5 @@ with mlflow.start_run():
     
 
 
-# Сохранение обученной модели
 joblib.dump(model, "iris_model.pkl")
 print("Модель сохранена как iris_model.pkl")
